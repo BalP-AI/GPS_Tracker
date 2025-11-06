ed0474f41b7b60e2c579e5e52911599b87297c8f
Valantis <balantispat@gmail.com>
2025-11-06 22:53:08 +0200

    basic testing added

--- gps_tracker/gps_tracker.ino
+++ gps_tracker/gps_tracker.ino
@@ -1,8 +1,31 @@
+#include <TinyGPSPlus.h>
+#include <HardwareSerial.h>
+
+#define RX 20
+#define TX 21
+
+TinyGPSPlus gps;
+HardwareSerial GPS(1); // Use UART1
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200); //START UART0 PC
+  GPS.begin(115200, SERIAL_8N1, RX, TX); // Start UART1 GPS
   Serial.println("Hello from ESP32-C3!");
 }
 
 void loop() {
   // Do nothing
+   while (GPS.available() > 0)
+    if (gps.encode(GPS.read()))
+      showdata();
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
+  }
 }
+
+void showdata()
+{
+  Serial.println("No data to show for now");
+}
+
